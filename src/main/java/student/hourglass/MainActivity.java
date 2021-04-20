9605f429c029b849d7a7b893bd158a365fd63266
smayfield2 <smayfield2@csub.edu>
2021-04-20 10:50:59 -0700

    initial commit of hourglass timer app

---
+++ src/main/java/student/hourglass/MainActivity.java
@@ -0,0 +1,14 @@
+package student.hourglass;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
